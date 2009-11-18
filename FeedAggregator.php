a9a03c6d0a012c2a8b5f6441f55f8a2b64c1ad1b
Isofarro <isofarro2@gmail.com>
2009-11-18 08:12:02 +0000

    configuration setup and pdo storage class start

--- FeedAggregator.php
+++ FeedAggregator.php
@@ -1,7 +1,24 @@
 <?php
 
 class FeedAggregator {
+	// Configuration
+	var $config = array(
+		'datasource' => 'sqlite:/tmp/db-aggregator.db'
+	);
+	
+	// Aggregator storage
+	var $storage;
 
+	public function __construct($config=false) {
+		if ($config) {
+			$this->setConfig($config);
+		}
+	}
+	
+	public function setConfig($config) {
+		$this->config = $config;
+	}
+	
 
 	public function getItems($url, $maxItems=5) {
 	
@@ -9,6 +26,17 @@ class FeedAggregator {
 	
 	public function getFeed($url, $maxItems) {
 	
+	}
+	
+	
+	##
+	## Protected methods
+	##
+	
+	protected function _initStorage() {
+		if(empty($this->storage)) {
+			$this->storage = new FeedAggregatorPdoStorage($this->config);
+		}
 	}
 
 }
